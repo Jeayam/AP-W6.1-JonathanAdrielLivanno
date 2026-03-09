f15f2333d7b55adf2fc1b5110c4f526bd41f139c
Jeayam <jlivanno@student.ciputra.ac.id>
2026-03-09 12:24:10 +0800

    pembuatan folder nomor

---
+++ Nomor 2/src/Nomor1.java
@@ -0,0 +1,22 @@
+
+        class Animal {
+            void sound() {
+            System.out.println("Some animal sound");
+            }
+            }
+
+        class Cat extends Animal {
+            @Override
+            void sound() {
+            System.out.println("Meow");
+            }
+            }
+
+public class Nomor1 {
+     public static void main(String[] args) {
+            Animal a = new Cat();
+            a.sound();
+            }
+            }
+            
+
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+Nomor 1
+Ouput :
 ![Output ScreenShot](images/Screenshotnomor1.png)
+Alasan : Karena methode sound di class animal di override dengan methode sound di class cat, yaitu extendnya class animal. Dan pada bagian main ada "Animal a = new Cat();" itu membuat animal sebagai variabel dan cat sebagai obeject dari a. Dan yang di run adalah a.sound
+
+Nomor 2
+Ouput
